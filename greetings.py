0b34a4e4aa125f8a31fa7aae8a220c47312f1787
eljorge700 <evojorge88@gmail.com>
2024-12-25 14:05:51 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,19 @@
+# имя пользователя
+username = 'George'
+
+# заголовок заметки
+title = 'Grade 1'
+
+# описание заметки
+content = 'Изучение первого модуля'
+
+# статус заметки
+status = 'in progress'
+
+# дата создания заметки
+created_date = '25-12-2024'
+
+# дата истечения заметки
+issue_date = '01-01-2025'
+
+print(username, title, content, status, created_date, issue_date, sep='\n')
