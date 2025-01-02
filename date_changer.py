2f322adbe28775e34f21c490f2d2871077a567a3
eljorge700 <evojorge88@gmail.com>
2025-01-02 15:32:20 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -10,6 +10,7 @@ issue_month = int(input("Enter issue month: "))
 issue_day = int(input("Enter issue day: "))
 issue_date = f'Issue date: "{issue_day:02d}-{issue_month:02d}"'
 temp_issue_date = datetime(current_year, issue_month, issue_day)
+
 print(created_date, issue_date, sep='\n')
 print(f'Temp created date: {temp_created_date}')
 print(f'Temp issue date: {temp_issue_date}')
